76103e1eba1668fffc37afa6b8fa1aae73f7751c
beastpredece <xjs5065@psu.edu>
2020-09-18 01:13:18 +0000

    3

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def sum_n(n):
-  if n==1:
-   return n
+  if n==0:
+   return 0
   else:
     return n + sum_n(n-1)
 
 def print_n(s,n):
-  if n==1:
-   print(s)
+  if n==0:
+   return
   else:
      print(s) 
      print_n(s,n-1)
