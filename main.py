ed2d6ae9eca6eb5e154fde655082686dc5a73e4d
beastpredece <xjs5065@psu.edu>
2020-09-18 01:29:32 +0000

    3

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+#Author: xuanyi shen xjs5065@psu.edu
+# Collaborator: Zihao Xu     zbx5084@psu.edu
+# Collaborator: Jared Cole jcc6066@psu.edu
+# Collaborator: Jacob Hallowell jph5997@psu.edu
+# Section: 4
+# Breakout: 9
+
 def sum_n(n):
   if n==0:
    return 0
